e84d8171a30fab829336fcfdec5c5a4bcbcf9bef
aviaga <56097089+aviaga@users.noreply.github.com>
2026-05-25 11:29:46 -0700

    Update run_fastngsadmix.sh

--- run_fastngsadmix.sh
+++ run_fastngsadmix.sh
@@ -2,18 +2,18 @@
 #$ -N run_admix
 #$ -cwd
 #$ -l h_data=4G,h_rt=02:00:00
-#$ -pe shared 1
+#$ -pe shared 4
 #$ -m bea
 #$ -o admix.log
 #$ -e admix.err
 
 source /u/local/Modules/default/init/bash
 
-mkdir -p ../results
+mkdir -p results
 
-../fastNGSadmix/fastNGSadmix \
-  -likes ../ANGSD_results/DRR608889_final.beagle.gz \
-  -fname ../snp_panel/fastNGSadmix_ref.txt \
-  -Nname ../snp_panel/nInd.txt \
-  -out ../results/admixResults \
+fastngsadmix/fastNGSadmix/fastNGSadmix \
+  -likes ANGSD_results/DRR608889_final.beagle.gz \
+  -fname fastNGSadmix_ref.txt \
+  -Nname nInd.txt \
+  -out results/admixResults \
   -whichPops all
