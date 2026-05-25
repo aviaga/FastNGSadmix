e491506744ba858e4bdf997172b8496cfb66774c
aviaga <56097089+aviaga@users.noreply.github.com>
2026-05-24 17:54:07 -0700

    Create run_angsd.sh

---
+++ run_angsd.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#$ -N run_angsd
+#$ -cwd
+#$ -l h_data=8G,h_rt=12:00:00
+#$ -pe shared 4
+#$ -m bea
+#$ -o angsd.log
+#$ -e angsd.err
+
+# File Paths (Relative to project root)
+RAW_SNPS="snp_panel/SNPs.txt"
+SITES_FILE="snp_panel/SNPs.sites4ANGSD.txt"
+BAM_FILE="bam/DRR608889.sorted.bam"
+OUT_DIR="ANGSD_results"
+ANGSD_EXEC="angsd/angsd"
+
+mkdir -p $OUT_DIR
+
+awk -F':' '{ print $1, $2, $3, $4 }' $RAW_SNPS > $SITES_FILE
+
+$ANGSD_EXEC sites index $SITES_FILE
+
+$ANGSD_EXEC \
+  -i $BAM_FILE \
+  -GL 2 \
+  -doGlf 2 \
+  -doMajorMinor 3 \
+  -doMaf 1 \
+  -minMapQ 0 \
+  -minQ 0 \
+  -sites $SITES_FILE \
+  -out $OUT_DIR/DRR608889_final
