54c76cecafc8d212fe1587aeec3785b647c4870b
aviaga <56097089+aviaga@users.noreply.github.com>
2026-05-27 19:44:59 -0700

    Update download_data.sh

--- download_data.sh
+++ download_data.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+#$ -N download_sra
+#$ -cwd
+#$ -l h_data=16G,h_rt=24:00:00
+#$ -pe shared 8
+#$ -m bea
+#$ -o download.log
+#$ -e download.err
+
+source /u/local/Modules/default/init/bash
+
+cd $SGE_O_WORKDIR/raw_fastq
+
 module load sra-tools/2.10.9
-prefetch DRR608889
-fasterq-dump DRR608889/DRR608889.sra -O ./ --split-files --threads 8 --progress
+prefetch $1
+fasterq-dump $1/$1.sra -O ./ --split-files --threads 8 --progress
