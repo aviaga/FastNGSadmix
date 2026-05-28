175422acd73b30b9d4339dc8b58499a815e452a7
aviaga <56097089+aviaga@users.noreply.github.com>
2026-05-27 19:45:24 -0700

    Update bwa_align.sh

--- bwa_align.sh
+++ bwa_align.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
-#$ -N bwa_align_dog
+#$ -N bwa_align
 #$ -cwd
 #$ -l h_data=32G,h_rt=24:00:00
 #$ -pe shared 4
 #$ -m bea
-#$ -o bwa_align.log
-#$ -e bwa_align.err
+#$ -o align.log
+#$ -e align.err
 
 source /u/local/Modules/default/init/bash
 module load bwa
 module load samtools
 
+cd $SGE_O_WORKDIR/bam
+
 bwa mem -t 4 \
   ../reference/UU_Cfam_GSD_1.0_ROSY.fa \
-  ../raw_fastq/DRR608889_1.fastq \
-  ../raw_fastq/DRR608889_2.fastq | \
-  samtools view -@ 2 -bS - > DRR608889.bam
+  ../raw_fastq/$1_1.fastq \
+  ../raw_fastq/$1_2.fastq | \
+  samtools view -@ 2 -bS - > $1.bam
 
-samtools sort -@ 2 -o DRR608889.sorted.bam DRR608889.bam
-samtools index DRR608889.sorted.bam
+samtools sort -@ 2 -o $1.sorted.bam $1.bam
+samtools index $1.sorted.bam
